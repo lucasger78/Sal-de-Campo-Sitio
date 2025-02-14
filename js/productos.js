84af134ebc42ddbebe6d9ff52e954fa21e03263b
lucasger78 <lucasger78@hotmail.com>
2025-02-14 01:47:38 -0300

    modificaciones producto y foto de portada

---
+++ js/productos.js
@@ -0,0 +1,47 @@
+// ver más original
+document.getElementById('ver-mas-entrefina').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('ingredientes-preview-entrefina').style.display = 'none'; 
+    document.getElementById('ingredientes-completos-entrefina').style.display = 'inline';
+    this.style.display = 'none'; 
+});
+
+// ver más fina
+document.getElementById('ver-mas-fina').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('ingredientes-preview-fina').style.display = 'none'; 
+    document.getElementById('ingredientes-completos-fina').style.display = 'inline';
+    this.style.display = 'none'; 
+});
+
+// ver más ahumada
+document.getElementById('ver-mas-ahumada').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('ingredientes-preview-ahumada').style.display = 'none'; 
+    document.getElementById('ingredientes-completos-ahumada').style.display = 'inline';
+    this.style.display = 'none'; 
+});
+
+// ver más 5 pimientas
+document.getElementById('ver-mas-pimientas').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('ingredientes-preview-pimientas').style.display = 'none'; 
+    document.getElementById('ingredientes-completos-pimientas').style.display = 'inline';
+    this.style.display = 'none'; 
+});
+
+// ver más suave
+document.getElementById('ver-mas-suave').addEventListener('click', function() {
+    document.getElementById('ingredientes-preview-suave').style.display = 'none'; 
+    document.getElementById('ingredientes-completos-suave').style.display = 'inline';
+    this.style.display = 'none'; 
+});
+
+// ver más especial
+document.getElementById('ver-mas-especial').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('ingredientes-preview-especial').style.display = 'none'; 
+    document.getElementById('ingredientes-completos-especial').style.display = 'inline';
+    this.style.display = 'none'; 
+});
+
